d92706552abc83318979f5f0c26b0ad2dc61b298
Vstukan <v.stukan14@gmail.com>
2024-07-24 16:17:42 +0300

    Add files via upload

---
+++ Module 4 1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as dif
+from true_math import divide as dim
+
+result1 = dif(69, 3)
+result2 = dif(3, 0)
+result3 = dim(49, 7)
+result4 = dim(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
